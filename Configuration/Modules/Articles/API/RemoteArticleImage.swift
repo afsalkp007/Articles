ca95da59b8f1e4f06d7db7098514bb518809a3a0
Afsal <afsalkp.mohammed@gmail.com>
2024-08-27 15:58:58 +0400

    Refacotoring `RemoteItemsMapper` by removing redundant code

--- Configuration/Modules/Articles/API/RemoteArticleImage.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Article.swift
-//  Articles
-//
-//  Created by Afsal Mohammed on 3/9/22
-//
-
-import Foundation
-
--- Configuration/Modules/Articles/API/RemoteItemsMapper.swift
+++ Configuration/Modules/Articles/API/RemoteItemsMapper.swift
@@ -8,93 +8,49 @@
 import Foundation
 
 final class RemoteItemsMapper {
-  struct ArticlesResponse: Decodable {
-    var articles: [Item]
-    
-    var images: [ArticleImage] {
-      return articles.map(\.image)
-    }
-    
-    enum CodingKeys: String, CodingKey {
-      case articles = "results"
-    }
-    
-    init(from decoder: Decoder) throws {
-      let container = try decoder.container(keyedBy: CodingKeys.self)
-      articles = try container.decodeValue(forKey: .articles)
-    }
-    
-    struct Item: Decodable {
-      var title: String
-      var author: String
-      var date: String
-      var description: String
-      var media: [Media]
-      
-      var image: ArticleImage {
-        return ArticleImage(
-          title: title,
-          author: author,
-          date: date,
-          description: description,
-          imageUrl: URL(string: media.first?.url ?? ""))
-      }
-      
-      enum CodingKeys: String, CodingKey {
-        case title
-        case author = "byline"
-        case date = "updated"
-        case desc = "abstract"
-        case media
-      }
-
-      init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        title = try container.decodeValue(forKey: .title)
-        author = try container.decodeValue(forKey: .author)
-        date = try container.decodeValue(forKey: .date)
-        description = try container.decodeValue(forKey: .desc)
-        media = try container.decodeValue(forKey: .media)
-      }
-      
-      struct Media: Decodable {
-        var metadata: [MetaData]
-        
-        var url: String? {
-          metadata.first?.imageUrl
-        }
-         
-        enum CodingKeys: String, CodingKey {
-          case metadata = "media-metadata"
-        }
-        
-        init(from decoder: Decoder) throws {
-          let container = try decoder.container(keyedBy: CodingKeys.self)
-          metadata = try container.decodeValue(forKey: .metadata)
-        }
-      }
-
-      struct MetaData: Decodable {
-        var imageUrl: String?
-        
-        enum CodingKeys: String, CodingKey {
-          case imageUrl = "url"
-        }
-        
-        init(from decoder: Decoder) throws {
-          let container = try decoder.container(keyedBy: CodingKeys.self)
-          imageUrl = try container.decodeValueIfPresent(forKey: .imageUrl)
-        }
-      }
-    }
-  }
-
-  
   static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [ArticleImage] {
-    guard let result = try? JSONDecoder().decode(ArticlesResponse.self, from: data), response.isOK else {
+    guard let result = try? JSONDecoder().decode(Articles.self, from: data), response.isOK else {
       throw RemoteArticlesLoader.Error.invalidData
     }
     
-    return result.images
+    return result.results.toModels()
   }
 }
+
+private struct Articles: Decodable {
+  let results: [RemoteArticleItem]
+}
+
+private struct RemoteArticleItem: Decodable {
+  let title: String
+  let byline: String
+  let updated: String
+  let abstract: String
+  let media: [Media]
+  
+  var url: String { media.first?.metadata.first?.url ?? "" }
+}
+
+private struct Media: Decodable {
+  let metadata: [MetaData]
+  
+  enum CodingKeys: String, CodingKey {
+    case metadata = "media-metadata"
+  }
+  
+  init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    metadata = try container.decodeValue(forKey: .metadata)
+  }
+}
+
+private struct MetaData: Decodable {
+  let url: String?
+}
+
+extension Array where Element == RemoteArticleItem {
+  func toModels() -> [ArticleImage] {
+    map { ArticleImage(title: $0.title, author: $0.byline, date: $0.updated, description: $0.abstract, imageUrl: URL(string: $0.url)) }
+  }
+}
+
